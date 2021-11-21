5ab308a548445aa68dac30cbdcb8e13b4c8cef9f
vs <vyacheslavshumkov@yandex.ru>
2021-11-21 17:52:23 +0500

    Add config

--- database.py
+++ database.py
@@ -11,13 +11,15 @@ async def init():
     vya_db = await User.filter(id=134238838).first()
     ron_db = await User.filter(id=1836505766).first()
 
-    if vya_db and not vya_db.is_admin:
-        vya_db.is_admin = True
+    if vya_db:
+        if not vya_db.is_admin:
+            vya_db.is_admin = True
     else:
         vya_db = User(id=134238838, first_name='Vyacheslav', is_admin=True)
 
-    if ron_db and not ron_db.is_admin:
-        ron_db.is_admin = True
+    if ron_db:
+        if not ron_db.is_admin:
+            ron_db.is_admin = True
     else:
         ron_db = User(id=1836505766, first_name='Niccolo', is_admin=True)
 
