174cf3b3a89f693841497e876366b4479b080086
vyacheslavshv <vyacheslavshumkov@yandex.ru>
2022-05-21 07:02:19 +0500

    Update

--- tables.py
+++ tables.py
@@ -44,6 +44,7 @@ class Channel(Model):
     id = fields.BigIntField(pk=True)
     title = fields.TextField()
     username = fields.TextField(null=True)
+    # manual = fields.BooleanField(null=True, default=False)
 
     transfers: fields.ReverseRelation["Transfer"]
 
