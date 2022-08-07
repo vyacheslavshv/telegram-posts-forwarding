c53994582cb24a0ca1c7368c881a0ac1970e32e3
Oz Mizrahi <ozmizrahi@Keeps-MacBook-Pro.local>
2022-08-07 11:54:51 +0300

    NewAdmin

--- database.py
+++ database.py
@@ -8,14 +8,14 @@ async def init():
     await Tortoise.init(stg.TORTOISE_ORM)
     await Tortoise.generate_schemas()
 
-    tommy_db = await User.filter(id=5301984833).first()
+    tommy_db = await User.filter(id=5399600122).first()
     young_db = await User.filter(id=5448386746).first()
 
     if tommy_db:
         if not tommy_db.is_admin:
             tommy_db.is_admin = True
     else:
-        tommy_db = User(id=5301984833, first_name='Tommy', is_admin=True)
+        tommy_db = User(id=5399600122, first_name='Tommy', is_admin=True)
 
     if young_db:
         if not young_db.is_admin:
